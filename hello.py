64b2634ce506ecc632f86c1fc8a53b165a70464e
SeanHu <seanhu1010@gmail.com>
2018-10-28 02:07:38 -0700

    Chapter 3: Custom error pages

--- hello.py
+++ hello.py
@@ -15,3 +15,13 @@ def index():
 def user(name):
     # return '<h1>Hello, {}!</h1>'.format(name)
     return render_template('user.html', name=name)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler
+def internal_server_error(e):
+    return render_template('500.html'), 500
